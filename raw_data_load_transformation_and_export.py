7963292a9cd325ee805fcc83d55cda3036ef237d
Jakub Masaryk <157375316+JakubMasaryk@users.noreply.github.com>
2025-06-18 13:25:38 +0200

    Update raw_data_load_transformation_and_export.py

--- raw_data_load_transformation_and_export.py
+++ raw_data_load_transformation_and_export.py
@@ -198,6 +198,6 @@ def scd_data_transformation_export(path, source, export_format):
     print(f"Data Transformation and Export complete. Files saved in: {base_dir}")
 
 #to transform and export the sample data as .xlsx (specify the exact folder)
-# sbw_data_transformation_export(path= r"", source= 'folder', export_format= 'xlsx')
-# scd_data_transformation_export(path= r"", source= 'folder', export_format= 'xlsx')
+# sbw_data_transformation_export(path= r".../sample_data_sbw", source= 'folder', export_format= 'xlsx')
+# scd_data_transformation_export(path= r".../sample_data_scd", source= 'folder', export_format= 'xlsx')
 
